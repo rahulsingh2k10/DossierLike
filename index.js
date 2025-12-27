7e5823cc88f09a92668a1cd6c393355dae7206ed
Rahul Singh <rahulsingh2k10@gmail.com>
2025-12-27 10:05:50 +0530

    added uuidv4

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
 app.use(express.json());
